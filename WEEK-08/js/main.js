deec4f05eba7f561efdfd7e40d87c4d1a43c2dbd
visualstudio <visualstudio@230-PC45>
2019-03-04 15:53:12 -0500

    forloop

--- WEEK-08/js/main.js
+++ WEEK-08/js/main.js
@@ -1,1 +1,16 @@
-﻿// Your code here!
+﻿let addnumber = {
+
+    starter =1109;
+    stepnumber = 11;
+    stopnumber =119
+    
+    adder: function () {
+        sum = 0;
+        for ()
+
+    }
+    return 
+
+}
+
+
